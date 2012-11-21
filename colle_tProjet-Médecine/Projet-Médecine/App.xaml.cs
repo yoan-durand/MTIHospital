4be30e77f0b6c5118cc3a768645c12add9bbb418
colle_t <thms.colle@gmail.com>
2012-11-21 16:05:10 +0100

    Adding architecture of the project

---
+++ colle_tProjet-Médecine/Projet-Médecine/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Projet_Médecine
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
