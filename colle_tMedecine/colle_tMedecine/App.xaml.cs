5d97b9c90df4355a058ddb29a86eaf26b52bff88
colle_t <thms.colle@gmail.com>
2012-12-01 18:39:49 +0100

    Adding navigation

--- colle_tMedecine/colle_tMedecine/App.xaml.cs
+++ colle_tMedecine/colle_tMedecine/App.xaml.cs
@@ -16,9 +16,19 @@ namespace colle_tMedecine
         {
             base.OnStartup(e);
             View.MainWindow window = new colle_tMedecine.View.MainWindow();
+
+            View.Login log = new colle_tMedecine.View.Login();
             ViewModel.LoginViewModel vm = new colle_tMedecine.ViewModel.LoginViewModel();
-            window.DataContext = vm;
+            log.DataContext = vm;
+            window.contentcontrol.Content = log;
+
+            View.Fiche_Patient menu = new colle_tMedecine.View.Fiche_Patient();
+            ViewModel.Fiche_PatientViewModel fm = new colle_tMedecine.ViewModel.Fiche_PatientViewModel();
+            menu.DataContext = fm;
+            window.contentcontrol.Content = menu;
+
             window.Show();
+            // Application.Current.MainWindow.DataContext;
         }
     }
 }
