cb29296f8ede978df4fc494a90aebdf009d8346e
yoan <durand.yoan@gmail.com>
2012-12-04 14:41:20 +0100

    ajout de la gestion selon profil

---
+++ colle_tMedecine/colle_tMedecine/Helper/BindingHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace colle_tMedecine.Helper
+{
+    class BindingHelper : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingHelper();
+        }
+
+        #endregion
+
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingHelper), new UIPropertyMetadata(null));
+    }
+}
